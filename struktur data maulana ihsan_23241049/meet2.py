6d69f464979e91f97054ba0fd98b27b491a44905
maulana ihsan <146051538+Ichsan909@users.noreply.github.com>
2024-04-01 12:00:39 +0800

    01 APRIL 2024

---
+++ struktur data maulana ihsan_23241049/meet2.py
@@ -0,0 +1,19 @@
+#beelajar variable
+#assigment
+x = 3
+y = 6
+uang = 1000
+
+#membuat pemanggilan
+print ("nilai uas saya =",x)
+print ("nilai uts saya =",y)
+print ("sisa uang saya =",uang)
+ 
+ #penamaan variabel
+nilai_a = 9
+kotak10 = 10
+
+#input variable ke dua
+
+x = 300
+print ("nilai uas saya =",x)

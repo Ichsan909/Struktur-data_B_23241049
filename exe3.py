d912d3d1367bdd6349574585c46ca1b1aa95d4b9
maulana ihsan <146051538+Ichsan909@users.noreply.github.com>
2024-04-22 11:45:14 +0800

    meet3 struktur data

---
+++ exe3.py
@@ -0,0 +1,27 @@
+#contoh type data (integer) : angka satuan
+bil_integer = 1
+print(type(bil_integer))
+
+print("Tipe data dari bil_integer adalah:", type(bil_integer))
+
+# contoh type data  (float) : bilangan desimal
+
+bil_float = 2.5
+
+print("Tipe data dari bil_integer adalah:", type(bil_float))
+ 
+
+#type data (string)
+
+bil_string = "m.ichsan"
+
+print("Tipe data dari bil_integer adalah:", type(bil_string))
+
+ #tipe data (bolean)
+
+x = 5
+y = 10
+
+bil_bol = x < y
+
+print("Tipe data dari bil_bol adalah:", type(bil_bol))

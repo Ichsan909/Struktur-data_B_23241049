d3f29766cd951aabed10eaacdd1a9c28fe88dad5
maulana ihsan <146051538+Ichsan909@users.noreply.github.com>
2024-04-29 11:51:22 +0800

    meet4&5

---
+++ meet4&5.py
@@ -0,0 +1,55 @@
+# input dari user
+# aritmatika
+
+
+# belajar inputan
+#maulana = input("masukkan kata : ")
+#print("isi dari maulana :",maulana,"bertipe data :", type(maulana))
+
+# belajar aritmatika dasar
+x = 11.5
+y = 4
+
+# penjumlahan +
+
+hasil = x + y
+
+print ("x + y =",hasil)
+
+# pengurangan
+
+hasil = x - y
+
+print ("x - y =",hasil)
+
+# perkalian
+hasil = x * y
+print ("x * y =",hasil)
+
+# pembagian
+hasil = x / y
+print ("x / y =",hasil)
+
+# perpangkatan exponent **
+hasil = x ** y
+print ("x ^ y =",hasil)
+
+# modulus %  (MODULUS ADALAH SISA BAGI)
+hasil = y % x
+print ("x mod y =",hasil)
+
+# floor division (pembulatan ke bawah)
+hasil = x // y
+print ("x // y =",hasil)
+
+# aritmatika prioritas
+# (),exponen,perkalian/pembagian,penjumlahan/pengurangan
+x = 3
+
+y = 4
+
+z = 5
+
+hasil = ( x ** y * (z + x) / y - z % x // y)
+print(hasil)
+

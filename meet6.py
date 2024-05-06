595801640fd9a1fe8854487b68452fdaf43aaefd
maulana ihsan <146051538+Ichsan909@users.noreply.github.com>
2024-05-06 11:42:43 +0800

    Add files via upload
    meet 6

---
+++ meet6.py
@@ -0,0 +1,41 @@
+x = 4
+y = 5
+
+# lebih bresar >
+hasil = x > y
+print(x, '>', y, 'adalah',hasil)
+
+# lebih kecil <
+hasil = x > y
+print(x, '<', y, 'adalah',hasil)
+
+# lebih besar >=
+hasil = x >= 2
+print(x, '>=', 2, 'adalah',hasil)
+print(x, '>=', 4, 'adalah',hasil)
+
+# lebih kecil <=
+hasil = x <= 2
+print(x, '<=', 2, 'adalah',hasil)
+print(x, '<=', 4, 'adalah',hasil)
+
+# sama dengan ==
+hasil = x == 7
+print(x, '==',7,'adalah',hasil)
+
+# tidak sama dengan !=
+hasil = x != 7
+print(x, '!=',7,'adalah',hasil)
+
+# sama "is" 
+# > < >= <= == != ini adalah perbandingan literal
+# x = 4, 4 = literal (tidak memakan memory)
+#
+hasil = x is 4
+print(x, 'is',y, 'adalah', hasil)
+
+# tidak sama "is not"
+hasil = x is not 4
+print(x, 'is not',4, 'adalah', hasil)
+
+

7cd9850d310192f247d41b5a02d7ad4a718c1561
maulana ihsan <146051538+Ichsan909@users.noreply.github.com>
2024-06-10 11:52:28 +0800

    meet 10

---
+++ meet10.py
@@ -0,0 +1,31 @@
+#PERBANDINGAN LANJUTAN [elif]
+
+nama = input("masukkan nama:")
+
+if nama=="edy":
+    print("dosen gsnteng")
+
+elif nama=="hariaonto":
+    print("dosen ganteng banget")
+
+elif nama=="ika":
+    print("dia mahasiswi")
+
+elif nama=="santoso":
+    print("masih salah")
+
+elif nama==("wario"):
+    print("masih salah")
+
+elif nama=="saikung": 
+    print("masih salah")
+
+elif nama=="budiono":
+    print("jawab yang bener lah") 
+
+elif nama=="alukard":
+    print("dah lah salah terus")              
+
+else:
+    print("bukan dosen anteng")
+    print("program drlrsai")        
